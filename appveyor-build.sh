e65449d28dffa8e5dc89f71dd364d55ecaa0e959
EXL <exlmotodev@gmail.com>
2018-03-08 03:52:38 +0700

    Fix build

--- appveyor-build.sh
+++ appveyor-build.sh
@@ -25,7 +25,9 @@ cd ../../
 
 sed -i "s/-Wl,--large-address-aware//g" lizus.pro
 sed -i "s/BOOST_LIB_SUFFIX=-mgw49-mt-s-1_57/BOOST_LIB_SUFFIX=-mt/g" lizus.pro
-sed -i "s/LIBS += -lboost_system-mgw49-mt-s-1_57 -lboost_filesystem-mgw49-mt-s-1_57 -lboost_program_options-mgw49-mt-s-1_57 -lboost_thread-mgw49-mt-s-1_57/LIBS += -lboost_system-mt -lboost_filesystem-mt -lboost_program_options-mt -lboost_thread-mt -lgmp/g" lizus.pro
+sed -i "s/LIBS += -lboost_system-mgw49-mt-s-1_57 -lboost_filesystem-mgw49-mt-s-1_57 -lboost_program_options-mgw49-mt-s-1_57 -lboost_thread-mgw49-mt-s-1_57/LIBS += -lboost_system-mt -lboost_filesystem-mt -lboost_program_options-mt -lboost_thread-mt/g" lizus.pro
+sed -i "s/BOOST_LIB_SUFFIX=-mgw49-mt-s-1_57/BOOST_LIB_SUFFIX=-mt/g" lizus.pro
+sed -i 's/LIBS += $$join(SECP256K1_LIB_PATH,,-L,) -lsecp256k1/LIBS += $$join(SECP256K1_LIB_PATH,,-L,) -lsecp256k1 -lgmp/g' lizus.pro
 sed -i "s/OS_WINDOWS_CROSSCOMPILE | NATIVE_WINDOWS)/OS_WINDOWS_CROSSCOMPILE | NATIVE_WINDOWS | MINGW64_NT-6.1 | MINGW32_NT-6.1 | MINGW32_NT-10.0 | MINGW64_NT-10.0 | MINGW32_NT-6.3 | MINGW64_NT-6.3)/g" src/leveldb/build_detect_platform
 make -j4 -C src/leveldb/ libleveldb.a libmemenv.a
 /mingw64/qt5-static/bin/qmake CONFIG+=release CONFIG+=static INCLUDEPATH+=/usr/local/include LIBS+='-static-libgcc -static-libstdc++ -L/usr/local/lib' lizus.pro
